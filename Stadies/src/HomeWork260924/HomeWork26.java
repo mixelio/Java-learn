d6f46b8c4846b9a51c65a678a9db5ace1b2679ae
Mykhailo Hubko <mixelios@gmail.com>
2024-09-27 00:12:45 +0200

    home work done

--- Stadies/src/HomeWork260924/HomeWork26.java
+++ Stadies/src/HomeWork260924/HomeWork26.java
@@ -1,6 +1,19 @@
 package HomeWork260924;
 
 public class HomeWork26 {
+    public static int addition (int a, int b) {
+        return a + b;
+    };
+    public static int substruction (int a, int b) {
+        return a - b;
+    };
+    public static int multiplication (int a, int b) {
+        return a * b;
+    };
+    public static double divide (int a, int b) {
+        return a / b;
+    };
+
     public static void main(String[] args) {
         String myString = new String("I study Basic Java!");
         System.out.println(myString.charAt(myString.length() - 2));
@@ -12,5 +25,13 @@ public class HomeWork26 {
         System.out.println(myString.replace("a", "o"));
         System.out.println(myString.toUpperCase());
         System.out.println(myString.toLowerCase());
+
+        int x = 25;
+        int y = 5;
+
+        System.out.println(addition(x, y));
+        System.out.println(substruction(x, y));
+        System.out.println(multiplication(x, y));
+        System.out.println(divide(x, y));
     }
 }
